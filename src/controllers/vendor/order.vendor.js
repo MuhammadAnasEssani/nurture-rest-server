23fc990a3f3128ee8d73fd84b242408c82308574
Muhammad Anas <anas4302537cloud.neduet.edu.pk>
2021-11-26 21:04:52 +0500

    change

--- src/controllers/vendor/order.vendor.js
+++ src/controllers/vendor/order.vendor.js
@@ -1,25 +1,75 @@
 const Order = require("../../models/order.js");
 
-// exports.updateOrder = (req, res) => {
-//   Order.updateOne(
-//     { _id: req.body.orderId, "orderStatus.type": req.body.type },
-//     {
-//       $set: {
-//         "orderStatus.$": [
-//           { type: req.body.type, date: new Date(), isCompleted: true },
-//         ],
-//       },
-//     }
-//   ).exec((error, order) => {
-//     if (error) return res.status(400).json({ error });
-//     if (order) {
-//       res.status(201).json({ order });
-//     }
-//   });
-// };
+exports.updateOrder = (req, res) => {
+  Order.updateOne(
+    { _id: req.body.orderId, "items.productId": req.body.productId },
+    {
+      $set: {
+        "items.$.itemStatus": true,
+      },
+    }
+  ).exec((error, order) => {
+    if (error) return res.status(400).json({ error });
+    if (order) {
+      // res.status(201).json({ order });
+      Order.findOne({ _id: req.body.orderId }).exec(async (error, ordered) => {
+        if (error) return res.status(400).json({ error });
+        if (ordered) {
+          // res.status(201).json({ ordered });
+          // ordered.items.forEach(function (val, index) {
+          //   if (val.itemStatus == false) {
+          //     console.log("Hello");
+          //     res.status(201).json({ message: "Orders Remaining" });
+          //     return;
+          //   } else {
+          //     console.log("Hello2");
+          //     Order.updateOne(
+          //       { _id: req.body.orderId, "orderStatus.type": "packed" },
+          //       {
+          //         $set: {
+          //           "orderStatus.$": [
+          //             { type: "packed", date: new Date(), isCompleted: true },
+          //           ],
+          //         },
+          //       }
+          //     ).exec((error, order) => {
+          //       if (error) return res.status(400).json({ error });
+          //       if (order) {
+          //         res.status(201).json({ order });
+          //       }
+          //     });
+          //   }
+          // });
+          const check = ordered.items.find(function (item) {
+            return item.itemStatus == false;
+          });
+          if (check == undefined) {
+            Order.updateOne(
+              { _id: req.body.orderId, "orderStatus.type": "packed" },
+              {
+                $set: {
+                  "orderStatus.$": [
+                    { type: "packed", date: new Date(), isCompleted: true },
+                  ],
+                },
+              }
+            ).exec((error, order) => {
+              if (error) return res.status(400).json({ error });
+              if (order) {
+                res.status(201).json({ order });
+              }
+            });
+          } else {
+            res.status(201).json({ message: "Orders Remaining" });
+          }
+        }
+      });
+    }
+  });
+};
 
 exports.getCustomerOrdersByVendors = async (req, res) => {
-  const orders = await Order.find({"items.productVendor": req.user._id})
+  const orders = await Order.find({ "items.productVendor": req.user._id })
     .populate("items.productId", "name")
     .exec((error, orders) => {
       if (error) return res.status(400).json({ error: "No Order Found" });
--- src/models/order.js
+++ src/models/order.js
@@ -1,85 +1,120 @@
 const mongoose = require("mongoose");
 const orderSchema = new mongoose.Schema(
-    {
-        user: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "User",
-            required: true,
-        },
-        addressId: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "UserAddress.address",
-            required: true,
-        },
-        totalAmount: {
-            type: Number,
-            required: true,
-        },
-        items: [
-            {
-                productVendor: {
-                    type: mongoose.Schema.Types.ObjectId,
-                    ref: "User",
-                    required: true,
-                },
-                itemStatus: [
-                    {
-                      type: {
-                        type: String,
-                        enum: ["ordered", "packed", "shipped", "delivered"],
-                        default: "ordered",
-                      },
-                      date: {
-                        type: Date,
-                      },
-                      isCompleted: {
-                        type: Boolean,
-                        default: false,
-                      },
-                    },
-                  ],
-                productId: {
-                    type: mongoose.Schema.Types.ObjectId,
-                    ref: "Product",
-                },
-                payablePrice: {
-                    type: Number,
-                    required: true,
-                },
-                purchasedQty: {
-                    type: Number,
-                    required: true,
-                },
-            },
-        ],
-        paymentStatus: {
-            type: String,
-            enum: ["pending", "completed", "cancelled", "refund"],
-            required: true,
-        },
-        paymentType: {
-          type: String,
-          enum: ["cod", "card"],
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    // addressId: {
+    //   type: mongoose.Schema.Types.ObjectId,
+    //   ref: "UserAddress.address",
+    //   required: true,
+    // },
+    totalAmount: {
+      type: Number,
+      required: true,
+    },
+    items: [
+      {
+        productVendor: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "User",
           required: true,
         },
-        orderStatus: [
-          {
-            type: {
-              type: String,
-              enum: ["ordered", "packed", "shipped", "delivered"],
-              default: "ordered",
-            },
-            date: {
-              type: Date,
-            },
-            isCompleted: {
-              type: Boolean,
-              default: false,
-            },
-          },
-        ],
+        itemStatus: {
+          type: Boolean,
+          default: false,
+
+        },
+        productId: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Product",
+        },
+        payablePrice: {
+          type: Number,
+          required: true,
+        },
+        purchasedQty: {
+          type: Number,
+          required: true,
+        },
+      },
+    ],
+    paymentStatus: {
+      type: String,
+      enum: ["pending", "completed", "cancelled", "refund"],
+      required: true,
     },
-    { timestamps: true }
+    paymentType: {
+      type: String,
+      enum: ["cod", "card"],
+      required: true,
+    },
+    address: {
+      // type: Object,
+      // required: true,
+      name: {
+        type: String,
+        required: true,
+        trim: true,
+        min: 3,
+        max: 50,
+      },
+      mobileNumber: {
+        type: String,
+        required: true,
+        trim: true,
+      },
+      pinCode: {
+        type: String,
+        required: true,
+        trim: true,
+      },
+      address: {
+        type: String,
+        required: true,
+        trim: true,
+        min: 10,
+        max: 100,
+      },
+      cityDistrictTown: {
+        type: String,
+        required: true,
+        trim: true,
+      },
+      state: {
+        type: String,
+        required: true,
+        required: true,
+      },
+      landmark: {
+        type: String,
+        min: 10,
+        max: 100,
+      },
+      alternatePhone: {
+        type: String,
+      }
+    },
+    orderStatus: [
+      {
+        type: {
+          type: String,
+          enum: ["ordered", "packed", "shipped", "delivered"],
+          default: "ordered",
+        },
+        date: {
+          type: Date,
+        },
+        isCompleted: {
+          type: Boolean,
+          default: false,
+        },
+      },
+    ],
+  },
+  { timestamps: true }
 );
 
 module.exports = mongoose.model("Order", orderSchema);
